cf0e8ab9b105633c14092c5cc38a570803233891
DhruvSvt <dhruvsvt08@gmail.com>
2024-05-30 12:51:25 +0530

    rf

--- resources/views/visitors/doctors-appointment.blade.php
+++ resources/views/visitors/doctors-appointment.blade.php
@@ -17,97 +17,49 @@
                 </div>
             </div>
         </div>
-        <div class="container">
-            <form action="#">
-                <div class="row">
-                    <div class="col-25">
-                        <label for="fname">First Name</label>
-                    </div>
-                    <div class="col-75">
-                        <input type="text" id="fname" name="firstname" required placeholder="Your name..">
-                    </div>
+      <div class="container">
+        <form action="/action_page.php">
+            <div class="row">
+                <div class="col-25">
+                    <label for="fname">First Name</label>
                 </div>
-                <div class="row">
-                    <div class="col-25">
-                        <label for="lname">Last Name</label>
-                    </div>
-                    <div class="col-75">
-                        <input type="text" id="lname" name="lastname" required placeholder="Your last name..">
-                    </div>
+                <div class="col-75">
+                    <input type="text" id="fname" name="firstname" placeholder="Your name..">
                 </div>
-                <div class="row">
-                    <div class="col-25">
-                        <label for="lname">Phone No</label>
-                    </div>
-                    <div class="col-75">
-                       <input type="text" id="phone" name="phone" required placeholder="Your phone number..">
-                    </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label for="lname">Last Name</label>
                 </div>
-                <div class="row">
-                    <div class="col-25">
-                        <label for="lname">Date</label>
-                    </div>
-                    <div class="col-75">
-                        <input type="date" id="phone" name="date" required placeholder="Your phone number..">
-                    </div>
+                <div class="col-75">
+                    <input type="text" id="lname" name="lastname" placeholder="Your last name..">
                 </div>
-                <div class="row">
-                    <div class="col-25">
-                        <label for="lname">City</label>
-                    </div>
-                    <div class="col-75">
-                        <input type="text" id="city" name="city" required placeholder="Your city..">
-                    </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label for="country">Country</label>
                 </div>
-                <div class="row">
-                    <div class="col-25">
-                        <label for="country">Departments</label>
-                    </div>
-                    <div class="col-75">
-                        <select id="country" name="country">
-                            <option value="">Please select department</option>
-                            <option value="neurology">Neurology</option>
-                            <option value="urology">Urology</option>
-                            <option value="gastroenterology">Gastroenterology</option>
-                            <option value="obstetrics and gynecology">Obstetrics and Gynecology</option>
-                            <option value="icu">ICU</option>
-                            <option value="nicu">NICU</option>
-                            <option value="surgery department">Surgery Department</option>
-                            <option value="gastrointestinal surgery">gastrointestinal surgery</option>
-                            <option value="pharmacy services">Pharmacy Services</option>
-                        </select>
-                    </div>
+                <div class="col-75">
+                    <select id="country" name="country">
+                        <option value="australia">Australia</option>
+                        <option value="canada">Canada</option>
+                        <option value="usa">USA</option>
+                    </select>
                 </div>
-                <div class="row">
-                    <div class="col-25">
-                        <label for="country">Doctor's</label>
-                    </div>
-                    <div class="col-75" style="margin-bottom: 16px;">
-                        <select id="country" name="country">
-                            <option value="">Select Your Doctor</option>
-                            <option value="dr. b. k. singh">Dr. B. K. Singh</option>
-                            <option value="dr. priyank singh">Dr. Priyank Singh</option>
-                            <option value="dr. priyank singh">Dr. Shilpi Dhakre</option>
-                            <option value="dr. priyank singh">Dr. Shivangi Goyal</option>
-                            <option value="dr. priyank singh">Dr. Kartikey Sharma</option>
-                            <option value="dr. priyank singh">Dr. Shalini Sharma</option>
-                            <option value="dr. priyank singh">Dr. S.K. Gupta</option>
-                            <option value="dr. priyank singh">Dr. Vijay Tyagi</option>
-                            <option value="dr. priyank singh">Dr. Deepshikha</option>
-                            <option value="dr. priyank singh">Dr. Vijay Pal Singh</option>
-                            <option value="dr. priyank singh">Dr. Rahul Sahai</option>
-                            <option value="dr. priyank singh">Dr. Alok Gupta</option>
-                            <option value="dr. priyank singh">Dr. Jitendra Srivastava</option>
-                            <option value="dr. priyank singh">Dr. Premashish Majumdar</option>
-                            <option value="dr. priyank singh">Dr. Anshuman Roy</option>
-                            <option value="dr. priyank singh">Dr. Virtul Gupta</option>
-
-                        </select>
-                    </div>
+            </div>
+            <div class="row">
+                <div class="col-25">
+                    <label for="subject">Subject</label>
                 </div>
-           <input type="submit" value="Submit">
-            </form>
-        </div>
+                <div class="col-75">
+                    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+                </div>
+            </div>
+            <div class="row1">
+                <input type="submit" value="Submit">
+            </div>
+        </form>
+    </div>
 
     <!--- END CONTAINER -->
 </section>
